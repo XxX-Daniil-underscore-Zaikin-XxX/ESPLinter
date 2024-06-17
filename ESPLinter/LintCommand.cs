ac1ff1bcc9c8f54d9f7782ad7c0c5d467ffcb10f
Daniil Zaikin <dan.zaikin@gmail.com>
2024-06-17 21:14:52 +1000

    initial commit

---
+++ ESPLinter/LintCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Plugins;
+
+//Sort of copied from Mutagen/Spriggit
+
+namespace ESPLinter
+{
+    [Verb("lint", HelpText = "Parses given plugin file for bad juju")]
+    public class LintCommand
+    {
+        [Option('i', "InputPath", HelpText = "Path to the Bethesda plugin file", Required = true)]
+        public string InputPath { get; set; } = string.Empty;
+
+        [Option('o', "OutputPath", HelpText = "Path to export the linted plugin. Leave blank to overwrite input", Required = false)]
+        public string OutputPath { get; set; } = string.Empty;
+
+        [Option('r', "GameRelease", HelpText = "The game release to target. Required.", Required = true)]
+        public GameRelease Release { get; set; }
+
+        [Option('v', "Verbose", HelpText = "Display each processed record. Off by default", Required = false)]
+        public bool Verbose { get; set; } = false;
+
+        [Option("DryRun", HelpText = "Setting to true prevents this program from modifying the plugin. false by default", Required = false)]
+        public bool DryRun { get; set; } = false;
+
+        [Option('p', "Pedantic", HelpText = "Fail on warnings. false by default", Required = false)]
+        public bool Pedantic { get; set; } = false;
+    }
+}
--- ESPLinter/Program.cs
+++ ESPLinter/Program.cs
@@ -1,2 +1,140 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using CommandLine;
+using ESPLinter;
+using Serilog;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Environments;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Aspects;
+using Mutagen.Bethesda.Plugins.Cache;
+using Mutagen.Bethesda.Plugins.Records;
+using Mutagen.Bethesda.Plugins.Records.Internals;
+using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Binary;
+using Mutagen.Bethesda.Plugins.Binary.Parameters;
+using CommandLine.Text;
+
+class Program
+{
+    static class LoggerSetup
+    {
+        public static ILogger Logger { get; }
+
+        static LoggerSetup()
+        {
+            Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Console(outputTemplate:
+                    "[{Timestamp:HH:mm:ss.fff} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+                .CreateLogger();
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        CommandLine.Parser.Default.ParseArguments<LintCommand>(args)
+            .WithParsed(RunLint)
+            .WithNotParsed(HandleParseError);
+    }
+
+
+    static void RunLint(LintCommand command)
+    {
+        var mod = SkyrimMod.CreateFromBinary(
+            ModPath.FromPath(command.InputPath),
+            command.Release.ToSkyrimRelease());
+        
+        ILinkCache scriptLinkCache = mod.ToMutableLinkCache();
+
+        foreach (var scripted in mod.EnumerateMajorRecords<IScripted>())
+        {
+            var scripts = scripted.VirtualMachineAdapter?.Scripts;
+            if (scripts == null || !scripts.Any())
+            {
+                continue;
+            }
+            foreach (var script in scripts)
+            {
+                if (script == null || !script.Properties.Any())
+                {
+                    continue;
+                }
+                script.Properties.Sort((prop1, prop2) => string.Compare(prop1.Name.ToLower(), prop2.Name.ToLower()));
+            }
+        }
+
+        /*foreach (var keyworded in mod.EnumerateMajorRecords<IKeyworded<IKeyword>>())
+        {
+            var keywords = keyworded.Keywords;
+            if (keywords == null || !keywords.Any())
+            {
+                continue;
+            }
+            keywords.Sort();
+        }*/
+        var writeParams = new BinaryWriteParameters()
+        {
+            ModKey = ModKeyOption.CorrectToPath,
+            
+
+        };
+        mod.WriteToBinary(
+            command.OutputPath,
+            writeParams);
+    }
+
+/*    static void LintMod<TMod>(GameRelease gameRelease, ModPath inputPath)
+        where TMod : IModDisposeGetter
+    {
+        using TMod importedMod = ModInstantiator<TMod>.Importer(inputPath, gameRelease);
+        //using var readOnlyInputMod = SkyrimMod.CreateFromBinaryOverlay(inputPath, SkyrimRelease.SkyrimSE);
+        foreach (var gamer in importedMod.EnumerateMajorRecords<IScripted>)
+    }
+*/
+
+    static void HandleParseError(IEnumerable<Error> errors)
+    {
+        /*var logger = LoggerSetup.Logger;
+        foreach (var error in errors)
+        {
+            logger.Error(error);
+            Console.Error.WriteLine(error);
+        }*/
+        var sentenceBuilder = SentenceBuilder.Create();
+        var logger = LoggerSetup.Logger;
+        foreach (var error in errors)
+        {
+            logger.Error(sentenceBuilder.FormatError(error));
+        }
+    }
+}
+
+class Linter
+{
+    private LintCommand _options;
+
+    public Linter(LintCommand options)
+    {
+        _options = options;
+    }
+
+    public HashSet<RecordType> getApplicableRecordTypes()
+    {
+        return new HashSet<RecordType>();
+    }
+
+    public bool filterRecord(IMajorRecordGetter record)
+    {
+        return false;
+    }
+
+    public int checkProblem(IMajorRecordGetter record)
+    {
+        return 0;
+    }
+
+    public bool fixProblem(IMajorRecordGetter record)
+    {
+        return true;
+    }
+}
